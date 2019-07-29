a73b726e4ec4dc7d400fc3e473d78fdd0050ee14
Ron Planken <ron.planken@pon.com>
2019-07-29 12:28:20 +0200

    updated weather script

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ from time import sleep
 
 rgb.background((0,0,0))
 rgb.clear()
-rgb.framerate(1)
+rgb.framerate(20)
 
 WEATHER_API_SERVER = 'https://api.openweathermap.org/data/2.5/weather?id=2745726&mode=json&units=metric&appid=100135c6eb0eeba9bde50e165d4021c1'
 
@@ -14,33 +14,34 @@ weather = None
 last_update = 0
 tick = 0
 
-REFRESH_RATE = 310
+REFRESH_RATE = 31
 
 rgb.setfont(0)
 
-if not wifi.status():
-    data, size, frames = animation_connecting_wifi
-    rgb.clear()
-    rgb.framerate(3)
-    rgb.gif(data, (12, 0), size, frames)
-    wifi.connect()
-    if wifi.wait():
-        rgb.clear()
-        rgb.framerate(20)
-    else:
-        rgb.clear()
-        rgb.framerate(20)
-        data, frames = icon_no_wifi
-        rgb.image(data, (12, 0), (8,8))
-        sleep(3)
-        rgb.clear()
+def disconnect_wifi():
+    if wifi.status():
+        wifi.disconnect()
+        rgb.pixel((255, 0, 0), (31, 0))  # red for no wifi
+
+def connect_wifi():
+    rgb.pixel((255, 255, 0), (31, 0))  # yellow connecting to wifi
+    if not wifi.status():
+        wifi.connect()
+        if wifi.wait():
+            rgb.pixel((0, 255, 0), (31, 0))  # green for wifi
+            rgb.framerate(20)
+        else:
+            rgb.clear()
+            rgb.framerate(20)
+            data, frames = icon_no_wifi
+            rgb.image(data, (12, 0), (8,8))
+            time.sleep(3)
+            rgb.clear()
+            machine.reboot()
+        
 
 def clear():
     rgb.clear()
-
-if not wifi.status():
-    print("Error connecting to wifi")
-    system.reboot()
 
 def input_B(pressed):
     global action
@@ -62,32 +63,44 @@ buttons.register(defines.BTN_B, input_B)
 buttons.register(defines.BTN_UP, input_up)
 buttons.register(defines.BTN_DOWN, input_down)
 
-while True:
-    if not wifi.status():
-        if not uinterface.connect_wifi():
-            system.reboot()
+gc.collect()
 
+while True:
     if tick < REFRESH_RATE:
         gc.collect()
         sleep(0.1)
-        rgb.pixel((150, 150, 0), (int(round(tick / 10)), 7))
+        rgb.pixel((150, 150, 0), (int(round(tick)), 7))
         tick += 1
         continue
     else:
         tick = 0
 
-    result = urequests.get(WEATHER_API_SERVER)
-    if result.status_code == 200:
-        rgb.pixel((0, 255, 0), (31, 7))  # green for new data
-        try:
-            weather = result.json()
-        except:
+    try:
+        connect_wifi()
+        print('retrieving result')
+        result = urequests.get(WEATHER_API_SERVER)
+        disconnect_wifi()
+        if result.status_code == 200:
+            rgb.pixel((0, 255, 0), (31, 7))  # green for new data
+            try:
+                print(gc.mem_free())
+                weather = result.json()
+            except:
+                print('Error during json parse')
+                print(result.text())
+                rgb.pixel((255, 0, 0), (31, 7))  # red for error
+        else:
+            print('Status: ' + result.status_code)
             rgb.pixel((255, 0, 0), (31, 7))  # red for error
-    else:
-        rgb.pixel((255, 0, 0), (31, 7))  # red for error
-        rgb.text('E {}'.format(result.status_code))
+            rgb.text('E {}'.format(result.status_code))
+    except Exception as e:
+        text = str(e)
+        print(text)
+        wifi.disconnect()
+        rgb.pixel((255, 0, 0), (31, 0))  # red for error
+        rgb.setfont(1)
+        rgb.text('No wifi')
+        sleep(10)
         
+    
     rgb.text(str(int(weather["main"]["temp"])), (255,255,255), (11,1))
-
-
-system.reboot()
